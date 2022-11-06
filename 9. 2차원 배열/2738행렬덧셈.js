947592ec9b2062dc3fbb9eed65f7fdffcff1cc9c
yurim <dding.yull@gmail.com>
2022-11-06 22:34:04 +0900

    행렬덧셈셈ing

---
+++ 9. 2차원 배열/2738행렬덧셈.js
@@ -0,0 +1,30 @@
+//2738 행렬 덧셈
+
+const answer = 
+[[3, 3]
+[1, 1, 1]
+[2, 2, 2]
+[0, 1, 0]
+[3, 3, 3]
+[4, 4, 4]
+[5, 5, 100]]
+
+const input = answer.split(' ');
+console.log(input);
+
+const all = input[0]
+const hang = all.split(' ')[0]
+const yull = all.split(' ')[1]
+
+
+
+const b = input[1]
+const first = b[0]
+const second = b[1]
+
+all.map(() => {
+
+})
+
+
+
