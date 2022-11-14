3106dca2f3a2997e64410208e56e1e623ed7b860
yurim <dding.yull@gmail.com>
2022-11-14 11:47:00 +0900

    2587 대표값2

--- 10.정렬/2587대표값2.js
+++ 10.정렬/2587대표값2.js
@@ -1,3 +1,9 @@
 // 2587 대표값
 
 let input = require('fs').readFileSync('/dev/stdin').toString().split('\n').map(Number);
+
+for(let i = 0; i < input.length; i++) {
+  if(input[i-1] < input[i]){
+    
+  }
+}
