3f2a5883de938e3c27000c8e9353c6f1f3b9c4d8
yurim <dding.yull@gmail.com>
2022-11-23 22:34:24 +0900

    10989수 정렬하기-ing

--- 10.정렬/10989수정렬하기3.js
+++ 10.정렬/10989수정렬하기3.js
@@ -2,3 +2,18 @@
 
 let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
 input.shift();
+
+for (let i = 0; i <= max; i++) {
+  for (let j = 0; j < input.length; j++) {
+    if (input[j] === i) {
+      count++;
+    }
+  }
+  newArr[i] = count;
+  if (count !== 0) {
+    for (let k = 0; k < newArr[i]; k++) {
+      console.log(i);
+    }
+  }
+  count = 0;
+}
