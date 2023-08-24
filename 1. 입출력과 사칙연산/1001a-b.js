8b3714b3300555e92e9d7c368c981efea9e41c13
yurim <dding.yull@innerwave.co.kr>
2023-08-24 09:59:43 +0900

    1001완료

--- 1. 입출력과 사칙연산/1001a-b.js
+++ 1. 입출력과 사칙연산/1001a-b.js
@@ -1,3 +1,7 @@
 const fs = require("fs");
-const inputData1 = fs.readFileSync("/dev/stdin").toString().split(" ");
-const inputData2 = fs.readFileSync(0, "utf8").split(" ");
+const inputData = fs.readFileSync(0, "utf8").toString().split(" ");
+
+const A = parseInt(inputData[0]);
+const B = parseInt(inputData[1]);
+
+console.log(A - B);
